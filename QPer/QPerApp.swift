519cc0cc0291aba1ec14be11a67f61a00cbbf0c7
cndrtan <andra_boyz@ymail.com>
2022-09-23 11:26:30 +0700

    initial commit

---
+++ QPer/QPerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QPerApp.swift
+//  QPer
+//
+//  Created by Candra Winardi on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct QPerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
